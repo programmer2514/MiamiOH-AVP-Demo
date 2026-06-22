0e242864199fed794d37718d44abc44c1a2f9cf4
Benjamin Pryor <43104632+programmer2514@users.noreply.github.com>
2026-06-21 23:24:26 -0400

    Interpreter refactor & animation revamp

---
+++ assets/interp-BGl9r8kt.js
@@ -0,0 +1,13 @@
+import{J as e,K as t,o as n,t as r}from"./jsx-runtime-CKpwozJo.js";import{i}from"./dist-BnOy0HyQ.js";import{t as a}from"./useInterpreter-D0FB77nB.js";var o=e(t(),1),s=r(),c=`a1 = arr[1, 10, 5, 6, 4, 7, 19]
+key = 4
+
+fun Linear_Search(a, k):
+    for n in a:
+        if n == k:
+            return n
+        end if
+    end for
+    return -1
+end fun
+
+Linear_Search(a1, key)`,l=n(function(){let[e,t]=(0,o.useState)(c),[n,r]=(0,o.useState)(`js`),[l,u,d,f]=a(e);return(0,s.jsxs)(`div`,{className:`overflow-y-scroll max-h-screen`,children:[(0,s.jsxs)(`div`,{children:[(0,s.jsx)(`h2`,{children:`Input Pseudocode`}),(0,s.jsx)(`textarea`,{value:e,onChange:e=>t(e.target.value),spellCheck:`false`})]}),(0,s.jsxs)(`div`,{children:[(0,s.jsx)(`h2`,{children:`Output`}),(0,s.jsxs)(`div`,{children:[(0,s.jsx)(i,{onClick:()=>r(`js`),children:`JavaScript`}),(0,s.jsx)(i,{onClick:()=>r(`tree`),children:`Parse Tree`}),(0,s.jsx)(i,{onClick:()=>r(`trace`),children:`Execution Trace`})]})]}),(0,s.jsx)(`pre`,{children:f||(n===`js`?l:n===`tree`?u:d)})]})});export{l as default};
